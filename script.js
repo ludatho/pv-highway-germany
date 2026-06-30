21063b32d322a673a123e269b2f8492be3168d52
ludatho <lucada.thomas@gmail.com>
2026-06-30 11:28:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const map = new maplibregl.Map({
   container: "map",
-  style: "https://demotiles.maplibre.org/style.json",
+  style: {
+    version: 8,
+    sources: {
+      osm: {
+        type: "raster",
+        tiles: [
+          "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        ],
+        tileSize: 256,
+        attribution: "© OpenStreetMap contributors"
+      }
+    },
+    layers: [
+      {
+        id: "osm",
+        type: "raster",
+        source: "osm"
+      }
+    ]
+  },
   center: [10.45, 51.16],
   zoom: 5.35
 });
